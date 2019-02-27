ce49eebf1403b89f934db339566d3a1f045f4bee
Starrier <1342878298@qq.com>
2019-02-27 16:41:19 +0800

    init

---
+++ src/main/java/org/starrier/nettyflower/NettyflowerApplication.java
@@ -0,0 +1,13 @@
+package org.starrier.nettyflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyflowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyflowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/starrier/nettyflower/NettyflowerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.starrier.nettyflower;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NettyflowerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
